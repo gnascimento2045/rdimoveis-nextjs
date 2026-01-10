212a300411713c933a880f1ed15c0a179aa798bd
GabrielPC <kira.msd10@gmail.com>
2026-01-10 03:21:24 -0300

    Add sitemap.xml and reference in robots.txt

---
+++ next-sitemap.config.js
@@ -0,0 +1,11 @@
+/**
+ * next-sitemap config
+ * Update `siteUrl` to your production domain before generating.
+ */
+module.exports = {
+  siteUrl: 'https://www.rdimoveisdf.com',
+  generateRobotsTxt: true,
+  changefreq: 'daily',
+  priority: 0.7,
+  sitemapSize: 7000,
+}
